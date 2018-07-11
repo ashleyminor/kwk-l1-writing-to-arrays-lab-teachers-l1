5d9adccff46effd788ea3d683229f6010a6a6af3
Ashley Minor <aminor@kipphouston.org>
2018-07-11 18:22:59 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -8,11 +8,12 @@ def change_rainbow_colors
   RAINBOW_COLORS[0] = "red"
   RAINBOW_COLORS[1] = "light_red"
   RAINBOW_COLORS[2] = "light_yellow"
+  return RAINBOW_COLORS
 end
 
 
 def add_colors
   RAINBOW_COLORS << "green"
   RAINBOW_COLORS << "blue"
-  return RAINBOW_COLORS
+  
 end
